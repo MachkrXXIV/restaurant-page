0d8d0380f80efc4e5f3aebc915f67a71d580b142
Justin Mach <bachmach24k@gmail.com>
2022-10-15 17:45:54 -0500

    wip home-page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 // should just load modules
 import Rat from "./imgs/little-chef.jpeg";
+import Ambient from "./imgs/ambient.jpg";
 import "./styles/reset.scss";
 import "./styles/main.scss";
 
+// Create image objects
 const rat = new Image();
+const ambient = new Image();
+
+// Add src url
 rat.src = Rat;
+ambient.src = Ambient;
+
+// Query selectors
 const content = document.getElementById("content");
-content.appendChild(rat);
+const home = document.querySelector(".home");
+
+// Add images to
+home.appendChild(rat);
+
+rat.classList.add("home--img");
+
 console.log("is running!");
-console.log("run this shit");
