ee2c2267380e07f70e969c745f9c5260dec99d85
Justin Mach <bachmach24k@gmail.com>
2022-10-26 00:02:53 -0500

    header js v1

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-// should just load modules
-import Rat from "./imgs/little-chef.jpeg";
-import Ambient from "./imgs/ambient.jpg";
 import "./styles/reset.scss";
 import "./styles/main.scss";
+import loadHome from "./pages/home";
 
-// Create image objects
-const rat = new Image();
-const ambient = new Image();
+/**
+ * @desc On first load, creates container and start on home page
+ */
+function initializePage() {
+  const content = document.querySelector(".content");
+  const container = document.createElement("main");
 
-// Add src url
-rat.src = Rat;
-ambient.src = Ambient;
+  container.classList.add(".container__main");
 
-// Query selectors
-const content = document.getElementById("content");
-const containerHome = document.querySelector(".container__home");
-const home = document.querySelector(".home");
-const slogan = document.querySelector(".home__slogan");
-const order = document.querySelector(".home__order");
+  content.appendChild(container);
+  loadHome();
+}
 
-// Add to page
-home.append(slogan);
-home.appendChild(rat);
-home.append(order);
-
-// Add classes(styling)
-home.classList.add("home--shadow");
-slogan.classList.add("home__slogan");
-order.classList.add("home__order");
-
-// Add text
-slogan.textContent = "Little Chef is here to impress!";
-order.innerHTML =
-  'Order online for <br/> <span class="home__pick-up">pick-up</span> or <span class="home__delivery">delivery</span> today!';
-
-rat.classList.add("home--img");
-
-console.log("is running!");
+initializePage();
